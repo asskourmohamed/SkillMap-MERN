2b590f6b1af17992d7e47c35f4ce5653e58ca20d
ASSKOUR <asskourmohamed1@gmail.com>
2026-05-03 22:50:15 +0100

    fixing bugs

--- frontend/src/pages/Admin/DashboardPage.js
+++ frontend/src/pages/Admin/DashboardPage.js
@@ -96,7 +96,7 @@ const handleCardClick = (cardId) => {
       break;
     case 'mentorships':
       console.log('Voir les mentorats');
-      // Naviguer vers la page des mentorats
+      // Naviguer vers la page
       break;
     case 'engagement':
       console.log('Voir les statistiques détaillées');
