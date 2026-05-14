84b266776cf667633f727e434afc28946cecb195
ASSKOUR <asskourmohamed1@gmail.com>
2026-05-14 22:58:38 +0100

    fixing bugs

--- frontend/src/components/Common/ImageUploader.js
+++ frontend/src/components/Common/ImageUploader.js
@@ -39,7 +39,7 @@ const ImageUploader = ({
       return;
     }
 
-    // Validat
+    // Val
     if (file.size > maxSize * 1024 * 1024) {
       setError(`L'image ne doit pas dépasser ${maxSize}MB`);
       return;
