9678e7e042a5d37be22580b0d4b1411202dc1569
ASSKOUR <asskourmohamed1@gmail.com>
2026-05-20 23:53:32 +0100

    updates

--- frontend/src/components/Admin/SkillGapsTable.js
+++ frontend/src/components/Admin/SkillGapsTable.js
@@ -21,7 +21,7 @@ const SkillGapsTable = ({ skillGaps }) => {
     );
   }
 
-  // Grouper les compétences par
+  // Grouper les compétences
   const skillMap = new Map();
 
   skillsArray.forEach(skill => {
